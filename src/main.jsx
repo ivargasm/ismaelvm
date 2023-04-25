87fd038c62aa0a69a11d7840ab601db41ec66408
ivargasm <ivargasm@hotmail.com>
2023-04-25 16:02:31 -0600

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './scripts/menu.js'
+import '../public/language.json'
+import '../public/projects.json'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
