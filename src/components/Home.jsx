438118105e59390b17daa4b1e8188667abd4318a
ivargasm <ivargasm@hotmail.com>
2023-04-27 12:37:07 -0600

    cambios en imagen-ismael

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,31 @@
 /* eslint-disable react/prop-types */
-import ismael from '../assets/imgs/ismael-bg.png'
+import ismael_red from '../assets/imgs/ismael-bg-red.png'
+import ismael_blue from '../assets/imgs/ismael-bg-color-blue.png'
+import { useEffect, useState } from 'react'
 
-export const Home = ({esen}) => {   
+export const Home = ({esen}) => { 
+
+    let theme = localStorage.getItem("theme")
+
+    const [img_bg, setImg_bg] = useState(theme)
+    
+    
+    useEffect(()=>{
+        if(theme === 'dark'){
+            setImg_bg(ismael_red)
+        }else{
+            setImg_bg(ismael_blue)
+        }
+        console.log('cambie a: '+ theme)
+    },[theme, setImg_bg, img_bg])
 
     return (
         // inicio
         <section className="begin-me" id="begin">
             <div className="begin-wrapper">
                 <div className="img-container rellax" data-rellax-speed="3">
-                    <div className="frame"></div>
                     <div className="img">
-                        <img src={ismael} alt=""/>
+                        <img src={img_bg} alt=""/>
                     </div>
                 </div>
                 <div className="begin-content">
