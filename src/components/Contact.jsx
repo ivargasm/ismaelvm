dfa74bb00725914648ae0148297470d1a4a21de4
isvargasm <ismael.vargas@storecheck.com>
2024-09-18 17:54:56 -0600

    Actualizacion de email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ export const Contact = ({esen}) => {
         <section className="contact-me" id="contact">
             <p className="contact-title title">{esen.contact}</p>
             <p className="contact-message">
-                {esen.c_msg} <br /> <a href="mailto:contacto@ismaelvm.xyz">{esen.c_mail}</a>
+                {esen.c_msg} <br /> <a href="mailto:contacto@ivargasm.com">{esen.c_mail}</a>
             </p>
         </section>
     )
